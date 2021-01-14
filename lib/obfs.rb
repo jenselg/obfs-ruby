c6fba189e77285f9851bff901847cb5a0d14c7bd
Jensel Gatchalian <jensel.gatchalian@gmail.com>
2021-01-13 23:14:48 -0800

    add method_missing shell method

--- lib/obfs.rb
+++ lib/obfs.rb
@@ -1,6 +1,13 @@
 class OBFS
 
         def initialize
+            # arguments:
+            # name
+            # absolute path
+            #
+        end
+
+        def method_missing(m, *args, &block)
         end
 
 end
